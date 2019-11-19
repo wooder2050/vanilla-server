cb77ad8bcdaff48deb763126434949079304f61c
Kang Jae-Young <wooder2050@gmail.com>
2019-11-19 18:58:47 +0900

    test stop

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -7,29 +7,29 @@ var sinon = require("sinon");
 const app = require("../app");
 var httpMocks = require("node-mocks-http");
 
-describe("GET / login failed", () => {
-  it("should send status 401", done => {
-    request(app)
-      .get("/login/failed")
-      .expect(401)
-      .end((err, res) => {
-        expect(res.body.message).to.equal("user failed to authenticate.");
-        done();
-      });
-  });
-});
+// describe("GET / login failed", () => {
+//   it("should send status 401", done => {
+//     request(app)
+//       .get("/login/failed")
+//       .expect(401)
+//       .end((err, res) => {
+//         expect(res.body.message).to.equal("user failed to authenticate.");
+//         done();
+//       });
+//   });
+// });
 
-describe("GET / logout", () => {
-  it("should send status 200 and {authenticated: false}", done => {
-    request(app)
-      .get("/logout")
-      .expect(200)
-      .end((err, res) => {
-        expect(res.body.authenticated).to.equal(false);
-        done();
-      });
-  });
-});
+// describe("GET / logout", () => {
+//   it("should send status 200 and {authenticated: false}", done => {
+//     request(app)
+//       .get("/logout")
+//       .expect(200)
+//       .end((err, res) => {
+//         expect(res.body.authenticated).to.equal(false);
+//         done();
+//       });
+//   });
+// });
 //비동기, req mock 작업
 // describe("POST / posts/upload", () => {
 //   it("should send status 200 and { userPost: true}", done => {
